d87d32d091928c615137275e68997ac53627df6f
Son Nguyen Hong <son.nguyenhong1@ncc.asia>
2025-08-25 16:57:47 +0700

    Commit C# Basic

---
+++ C# Basic/ExampleDateTime2/Inlichthang.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ExampleDateTime
+{
+    class Program
+    {
+        static void Main()
+        {
+            DateTime now = DateTime.Now;
+            int year = now.Year;
+            int month = now.Month;
+
+            int daysInMonth = DateTime.DaysInMonth(year, month);
+            Console.WriteLine($"Lich thang {month}/{year}:");
+
+            for (int day = 1; day <= daysInMonth; day++)
+            {
+                DateTime d = new DateTime(year, month, day);
+                Console.WriteLine($"{d:dd/MM/yyyy} - {d.DayOfWeek}");
+            }
+        }
+    }
+}
---
+++ C# Basic/ExampleString3/VietHoaDauTu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Nhap mot cau: ");
+        string input = Console.ReadLine();
+
+        string[] words = input.Split(' ');//Chia cac tu cach nhau ' '
+        for (int i = 0; i < words.Length; i++)
+        {
+            string word = words[i];
+            if (word.Length > 0)
+            {
+                words[i] = char.ToUpper(word[0]) + word.Substring(1).ToLower();//Chuyen moi tu thanh viet hoa dau 
+            }
+        }
+
+        string result = string.Join(" ", words);
+        Console.WriteLine("Chuoi da viet hoa moi tu: " + result);
+    }
+}
