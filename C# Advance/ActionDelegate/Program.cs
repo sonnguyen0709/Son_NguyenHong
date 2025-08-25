5dc6bda244c0a2f3ae84139e864b50f24e08f7af
Son Nguyen Hong <son.nguyenhong1@ncc.asia>
2025-08-25 16:26:08 +0700

    Commit C# Advance

---
+++ C# Advance/ActionDelegate/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Example 1: ");
+        Example1.Run();
+        Console.WriteLine("Example 2: ");
+        Example2.Run();
+    }
+}
---
+++ C# Advance/AnonymousMethod/AnonymousMethod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Example1
+{
+    public static void Run()
+    {
+        Action greet = delegate
+        {
+            Console.WriteLine("Chao mung ban.");
+        };
+        greet();
+
+        Action<string> sayHello = delegate (string name)
+        {
+            Console.WriteLine($"Xin chao {name}");
+        };
+
+        Console.WriteLine("Nhap ten cua ban : ");
+        string input = Console.ReadLine();
+        sayHello(input);
+
+        ExecuteGreeting(delegate (string name)
+        {
+            Console.WriteLine($"Chuc ban mot ngay tot lanh, {name}!");
+        }, input);
+    }
+}
+
